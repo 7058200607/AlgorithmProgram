a3d203832d88b9454c6e1d27116db6f3b7675a77
vrushali joshi <vrushalijoshi589@mail.com>
2022-06-02 13:04:09 +0530

    prime Number added

---
+++ AlgorithmProgram/PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class PrimeNumber
+    {
+        public static void APrime() 
+        {
+
+            int count = 0;
+            Console.WriteLine("Prime Numbers between 1 to 1000 : ");
+            for (int num = 2; num <= 1000; num++)
+            {
+                for (int i = 2; i <= num / 2; i++)
+                {
+
+                    if ( num % i == 0)
+                    {
+                        count++;
+                    }
+
+                }
+                if (count == 0)
+                {
+                    Console.Write(num + " ");
+                }
+                count = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlgorithmProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class Program 
+    {
+        public static void Main(string [] args) 
+        {
+            Console.WriteLine("Enter a prime Number :");
+
+            PrimeNumber.APrime();
+            
+            
+        }
+
+    }
+}
+
